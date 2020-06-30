4ff54ea05e1ca178038328578765c306eb9734c6
Jonas dos Santos <jonas.cassiano@hotmail.com>
2020-06-30 13:26:55 +0200

    Google Analytics Subdomain Test

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import ReactGa from 'react-ga'
 
 const SimpleComponent = () => (
   <div>
     <p>This is a simple blog</p>
-    <a href="https://www.jonascsantos.com">Visit jonascsantos.com!</a>
+    <a href='https://www.jonascsantos.com' onClick={() => ClickHandler('mainWebsite')} >Visit jonascsantos.com!</a>
   </div>
 )
 
+
+
+const ClickHandler = name => {
+  ReactGa.event({
+    category: name,
+    action: 'Clicked'
+  })
+}
+
 function App() {
-  return <SimpleComponent />
+  useEffect(() => {
+    ReactGa.initialize('UA-152578899-1')
+    ReactGa.pageview('/')
+  }, [])
+  
+  return <SimpleComponent ClickHandler={ClickHandler}  />
 }
 
 export default App
