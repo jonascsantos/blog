01c099c4bef9870ca0c6488542fdb04a1ba99aad
Jonas dos Santos <jonas.cassiano@hotmail.com>
2020-06-30 12:00:53 +0200

    Second Commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
 
-const Spacer = styled.div`
-  padding-left: 2vw;
-`
-
 const SimpleComponent = () => (
   <div>
     <p>This is a simple blog</p>
