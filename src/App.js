20789993b90fa906b50c9636d387827c9756cfba
Jonas dos Santos <jonas.cassiano@hotmail.com>
2020-07-02 18:53:18 +0200

    Google Tag Manager

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import ReactGa from 'react-ga'
 const SimpleComponent = () => (
   <div>
     <p>This is a simple blog</p>
-    <a href='https://www.jonascsantos.com' onClick={() => ClickHandler('mainWebsite')} >Visit jonascsantos.com!</a>
+    <a href='https://www.jonascsantos.com' onClick={() => ClickHandler('mainWebsite')}>
+      Visit jonascsantos.com!
+    </a>
+    <button onClick={() => sendTiming('sendTime_Button')}>Send Time</button>
   </div>
 )
-
-
 
 const ClickHandler = name => {
   ReactGa.event({
@@ -17,13 +18,23 @@ const ClickHandler = name => {
   })
 }
 
+var megamenuTimingStart = +new Date();
+
+const sendTiming = label => {
+  var megamenuTimingEnd = +new Date();
+  var diff = Math.round((megamenuTimingEnd - megamenuTimingStart));
+  console.log(diff, ': ', label);
+  var ga = ReactGa.ga();
+  ga('send', 'event', 'Megamenu','Click_TimeToAction', label, diff);
+}
+
 function App() {
   useEffect(() => {
     ReactGa.initialize('UA-152578899-1')
     ReactGa.pageview('/')
   }, [])
-  
-  return <SimpleComponent ClickHandler={ClickHandler}  />
+
+  return <SimpleComponent ClickHandler={ClickHandler} />
 }
 
 export default App
