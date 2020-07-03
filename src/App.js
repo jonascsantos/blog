a140549414872c6d34fa4d755aaddee27f9d92df
Jonas dos Santos <jonas.cassiano@hotmail.com>
2020-07-03 17:32:17 +0200

    Blog React Route | Google Tag Manager Test

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import React, { useEffect } from 'react'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 import ReactGa from 'react-ga'
 
 const SimpleComponent = () => (
   <div>
     <p>This is a simple blog</p>
-    <a href='https://www.jonascsantos.com' onClick={() => ClickHandler('mainWebsite')}>
+    <a href='https://jonascsantos.com' onClick={() => ClickHandler('mainWebsite')}>
       Visit jonascsantos.com!
     </a>
     <button onClick={() => sendTiming('sendTime_Button')}>Send Time</button>
+  </div>
+)
+const About = () => (
+  <div>
+    <p>About</p>
+    <button onClick={() => sendEvent('AboutButton')}>About Button</button>
+  </div>
+)
+const Users = () => (
+  <div>
+    <p>Users</p>
+    <button onClick={() => sendEvent('UsersButton')}>Users Button</button>
+  </div>
+)
+const Home = () => (
+  <div>
+    <p>Home</p>
+    <button onClick={() => sendEvent('HomeButton')}>Home Button</button>
   </div>
 )
 
@@ -23,16 +42,18 @@ var megamenuTimingStart = +new Date()
 const sendTiming = label => {
   var megamenuTimingEnd = +new Date()
   var diff = Math.round(megamenuTimingEnd - megamenuTimingStart)
-  console.log(diff, ': ', label)
-  //var ga = ReactGa.ga()
   window.dataLayer = window.dataLayer || []
   window.dataLayer.push({
     'event': 'item-click',
     'eventLabel': label,
     'eventValue': diff
   })
-
-  //ga('send', 'event', 'Megamenu', 'Click_TimeToAction', label, diff)
+}
+const sendEvent = label => {
+  if(window.dataLayer)
+    window.dataLayer.push({
+      'event': label,
+    })
 }
 
 function App() {
@@ -41,7 +62,40 @@ function App() {
     ReactGa.pageview('/')
   }, [])
 
-  return <SimpleComponent ClickHandler={ClickHandler} />
+  return (
+    <Router>
+      <SimpleComponent ClickHandler={ClickHandler} />
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  )
 }
 
 export default App
